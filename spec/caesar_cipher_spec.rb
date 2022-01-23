06054eca2b2e33f0ce1b00d98c147f3459884f72
Lucas Furlanetto Pascoali <luccafpascoali@gmail.com>
2022-01-23 16:17:58 -0300

    refactored caesar_cipher_spec.rb

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -5,34 +5,30 @@ require './lib/caesar_cipher'
 
 # rubocop:disable Metrics/BlockLength
 describe CaesarCipher do
+  let(:caesar_cypher) { CaesarCipher.new }
+
   describe '#encrypt' do
     it 'returns the correct encryption with a more than 0 and less than 26 factor' do
-      caesar_cypher = CaesarCipher.new
       expect(caesar_cypher.encrypt('lucas', 5)).to eql('qzhfx')
     end
 
     it 'returns the correct encryption with a more than 26 factor' do
-      caesar_cypher = CaesarCipher.new
       expect(caesar_cypher.encrypt('lucas', 27)).to eql('mvdbt')
     end
 
     it 'returns the same string when the factor is 26' do
-      caesar_cypher = CaesarCipher.new
       expect(caesar_cypher.encrypt('lucas', 26)).to eql('lucas')
     end
 
     it 'returns the same string when the factor is 0' do
-      caesar_cypher = CaesarCipher.new
       expect(caesar_cypher.encrypt('lucas', 0)).to eql('lucas')
     end
 
     it 'returns the correct encryption when the factor is less than 0' do
-      caesar_cypher = CaesarCipher.new
       expect(caesar_cypher.encrypt('lucas', -1)).to eql('ktbzr')
     end
 
     it 'encryption with -1 is equal to encryption with 25' do
-      caesar_cypher = CaesarCipher.new
       expect(caesar_cypher.encrypt('lucas', -1)).to eql(caesar_cypher.encrypt('lucas', 25))
     end
   end
